23507b71fbf797a99955bc726885a754ec997cf9
tejaswini <tejaswinipinge154@gmail.com>
2023-01-21 21:07:40 -0500

    Added ArrayList class

---
+++ src/com/velocity/demo/Test.java
@@ -0,0 +1,15 @@
+package com.velocity.demo;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+	ArrayList<String> list=new ArrayList<String>();
+	list.add("java");
+	list.add("Hibernate");
+	list.add("Spring");
+	System.out.println(list);
+	}
+
+}
